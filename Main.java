372790781895ace5d0b146f3f77b51157680ccc5
Amrit <amritsahoo814@gmail.com>
2025-09-26 08:58:35 +0530

    my jenkins branch commit

--- Main.java
+++ Main.java
@@ -3,6 +3,6 @@ import java.util.Date;
 public class Main{
     public static void main(String[] args) {
         System.out.println("This is Java program for jenkins");
-         System.out.println("Date is: "+new Date());
+         System.out.println("Date is: "+new Date());//
     }
 }
